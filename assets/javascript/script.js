1967fc480ab46d3ac8c3be2b18c99d5d2038d45c
giobocca03 <gio.bocca03@gmail.com>
2025-01-21 19:07:08 +0100

    inserito controllo pokemon scelto su input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 
   const pokemonLista = document.querySelector("[data-pokemon]");
-
+  const pokemonNome = document.querySelector("[data-pokemon-name]");
+  const cercaPokemon = document.querySelector("[data-cerca-pokemon]")
+  let options = [];
   fetch("https://pokeapi.co/api/v2/pokemon?limit=151", {
     method: "GET"
   })
@@ -13,6 +15,7 @@
       let pokeapi = document.createElement("option");
       pokeapi.value = data.results[count].name;
       pokemonLista.appendChild(pokeapi); // Aggiungi l'opzione alla lista*/
+      options.push(data.results[count].name)
       count++
     }
   })
@@ -20,5 +23,13 @@
     console.log(errore);
   });
 
-
+cercaPokemon.addEventListener("click",()=>{
+  console.log(pokemonNome.value)
+  const inputVAl = pokemonNome.value.toLowerCase()
+  if(options.includes(inputVAl)){
+    console.log("ciao")
+  } else{
+    console.log("inserisci un nome esistente")
+  }
+})
 
