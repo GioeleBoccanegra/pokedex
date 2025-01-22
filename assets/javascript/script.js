674d673522496b5ab5556ec8a193b6d0743a2b1f
giobocca03 <gio.bocca03@gmail.com>
2025-01-22 09:23:59 +0100

    aggiunto funzioni per pulire input e div alla ricerca

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,27 @@
   const cercaPokemon = document.querySelector("[data-cerca-pokemon]")
   const spriteContainer = document.querySelector("[data-pokemon-sprite]");
   let options = [];
+
+function cleanInput(){
+  pokemonNome.value=""
+}
+  function cleanDiv(){
+    const pokeSprite = spriteContainer.querySelector("img");
+    if(pokeSprite){
+      spriteContainer.removeChild(pokeSprite);
+    }
+  }
+
+
+  function insertSprite (data){
+    let pokeSprite = document.createElement("img");
+    pokeSprite.src = data.sprites.front_default;
+    spriteContainer.appendChild(pokeSprite); // Aggiungi l'opzione alla lista
+  }
+
+
+
+
   fetch("https://pokeapi.co/api/v2/pokemon?limit=151", {
     method: "GET"
   })
@@ -28,18 +49,20 @@ cercaPokemon.addEventListener("click",()=>{
   console.log(pokemonNome.value)
   const inputVAl = pokemonNome.value.toLowerCase()
   if(options.includes(inputVAl)){
+    cleanInput()
+    cleanDiv()
     fetch(`https://pokeapi.co/api/v2/pokemon/${inputVAl}`, {
     method: "GET"
   }).then(response=>{
     return response.json()
   }).then(data=>{
     console.log(data.sprites.front_default)
-    let pokesprite = document.createElement("img");
-      pokesprite.src = data.sprites.front_default;
-      spriteContainer.appendChild(pokesprite); // Aggiungi l'opzione alla lista
+    insertSprite(data)
   })
   } else{
     console.log("inserisci un nome esistente")
   }
 })
+
+
 
